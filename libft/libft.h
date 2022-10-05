9046bb86dace988b3aa424d6744cb06b33786fb9
Khoubaib Boughalmi <khoubaib.kb99@gmail.com>
2022-10-05 16:58:20 +0200

    fixing .h file

--- libft/libft.h
+++ libft/libft.h
@@ -32,7 +32,7 @@ void	*ft_calloc(size_t count, size_t size);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
-void	ft_putendl_fd(char const *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
